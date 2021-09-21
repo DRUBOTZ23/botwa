f5e8054e737d9d651655ded48411b49d9013598b
Dru-230 <75634180+Dru-230@users.noreply.github.com>
2021-09-21 23:13:36 +0700

    Create mm_mb.js

---
+++ diamond/mm_mb.js
@@ -0,0 +1,13 @@
+const mm_mb = () => {
+return `[{
+										"title": "",
+										"rowId": "0"
+									},
+									{
+										"title": "",
+										"rowId": "0"
+									}]
+			`
+	}
+
+exports.mm_mb = mm_mb
